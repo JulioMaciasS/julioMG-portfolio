590676b6c0a0ac3a95b7d59b9bf02a2df85d19eb
JulioMaciasS <juliotabj@gmail.com>
2023-12-28 10:47:04 +0000

    icon name fixed

--- src/components/ProjectCards.js
+++ src/components/ProjectCards.js
@@ -9,7 +9,7 @@ function ProjectCards() {
     var CSSIconLink = './images/logos/css.png';
     var swiftIconLink = './images/logos/swift.png';
     var XcodeIconLink = './images/logos/Xcode.png';
-    var swiftUIIconLink = './images/logos/swiftUI.png';
+    var swiftUIIconLink = './images/logos/swiftui.png';
 
 
     return (
